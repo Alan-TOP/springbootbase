fcb949b8ffb5a19f8edc7abcdbac20cff2af05ed
alan <anshuaicn@163.com>
2019-05-21 22:56:14 +0800

    log4j,暂时存在问题，待修复

--- src/main/java/com/alan/springbootbase/controller/HelloRestController.java
+++ src/main/java/com/alan/springbootbase/controller/HelloRestController.java
@@ -1,5 +1,7 @@
 package com.alan.springbootbase.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,8 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("rest")
 public class HelloRestController {
 
+    Logger logger = LoggerFactory.getLogger(HelloRestController.class);
+
     @RequestMapping("hello")
     public String hello(){
+        logger.debug("rest hello");
         return "hello Alan";
     }
 }
