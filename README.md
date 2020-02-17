c52532e1523c5e9d7242e20736292a7f4e2d58a7
alan <anshuaicn@163.com>
2020-02-17 23:53:43 +0800

    增加了测试模块及开发测试环境配置文件分离

--- README.md
+++ README.md
@@ -2,3 +2,14 @@
 
 简介：
 基于springboot搭建一个基础开发框架
+该项目主要用于总结规整springboot所涉及到的相关技术
+
+
+
+目前项目已实现集成如下：
+
+
+#restController
+
+
+
---
+++ src/test/java/com/alan/springbootbase/controller/SysUserControllerTest.java
@@ -0,0 +1,41 @@
+package com.alan.springbootbase.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class SysUserControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    //MVC方式
+    @Test
+    public void findAllMVC() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/user/findAll"))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+
+    //传统方式
+    @Autowired
+    private SysUserController sysUserController;
+
+    @Test
+    public void findAll() {
+
+        System.out.println("***********"+sysUserController.findAll());
+
+    }
+}
